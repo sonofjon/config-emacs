2861d84300036b69076b29ee2435e2d501aa3d6f
Andreas Jonsson <ajdev8@gmail.com>
2021-03-29 22:04:06 +0200

    Add theme

---
+++ init.el
@@ -0,0 +1,8 @@
+;; Load theme
+;(load-theme 'dichromacy)
+;(load-theme 'manoj-dark)
+;(load-theme 'misterioso)
+;(load-theme 'tango-dark)
+(load-theme 'tsdh-dark)
+;(load-theme 'wheatgrass)
+;(load-theme 'wombat)
