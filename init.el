9afa0f65e6e1ebf5687d91ce1b7ffca013d0adc4
Andreas Jonsson <ajdev8@gmail.com>
2021-03-30 12:23:46 +0200

    Add melpa to package-archives

--- init.el
+++ init.el
@@ -6,3 +6,11 @@
 (load-theme 'tsdh-dark)
 ;(load-theme 'wheatgrass)
 ;(load-theme 'wombat)
+
+;; Initialize package sources
+(require 'package)
+(package-initialize)
+
+;; Add melpa to package-archives
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
