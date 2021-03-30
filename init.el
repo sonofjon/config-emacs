6d5dac74b57623803ecbf1ef32e41e7782be497d
Andreas Jonsson <ajdev8@gmail.com>
2021-03-30 12:45:15 +0200

    Use custom-file.el for custom-* code

--- init.el
+++ init.el
@@ -7,6 +7,12 @@
 ;(load-theme 'wheatgrass)
 ;(load-theme 'wombat)
 
+;; Use custom-file.el for custom-* code
+(setq custom-file "~/.emacs.d/custom-file.el")
+
+;; Load custom file
+(load-file custom-file)
+
 ;; Initialize package sources
 (require 'package)
 (package-initialize)
