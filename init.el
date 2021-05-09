586217fe39104c4d0e6f9f87ce6023fd9bac4697
Andreas Jonsson <ajdev8@gmail.com>
2021-05-09 17:10:46 +0200

    Add code to calculate and display startup time

--- init.el
+++ init.el
@@ -1,4 +1,19 @@
 ;;
+;; STARTUP
+;;
+
+;; Check startup time
+(defun efs/display-startup-time ()
+  (message "Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time
+                    (time-subtract after-init-time before-init-time)))
+           gcs-done))
+
+(add-hook 'emacs-startup-hook #'efs/display-startup-time)
+
+
+;;
 ;; INITIALIZATION
 ;;
 
