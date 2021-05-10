fd9e33de7b6de8156e6a59bcef21687bbaec57a9
Andreas Jonsson <ajdev8@gmail.com>
2021-05-10 10:11:36 +0200

    Wrap long lines

--- init.el
+++ init.el
@@ -91,6 +91,13 @@
 ;; PACKAGES
 ;;
 
+;; benchmark-init (startup profiler)
+(use-package benchmark-init
+  ;; :disabled
+  :config
+  ;; Disable collection of benchmark data after init
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 ;; auto-package-update
 (use-package auto-package-update
   :config
@@ -323,9 +330,12 @@
   (interactive)
   (ediff-copy-diff ediff-current-difference nil 'C nil
 		   (concat
-		    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
-		    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+		    (ediff-get-region-contents ediff-current-difference
+                                               'A ediff-control-buffer)
+		    (ediff-get-region-contents ediff-current-difference
+                                               'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map ()
+  (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
 (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
 
 ;; dired: custom listing style
