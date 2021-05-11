9d9aee44ca25434c11b2b099cf3082f6d7a9783b
Andreas Jonsson <ajdev8@gmail.com>
2021-05-11 13:01:56 +0200

    Add support for spacemacs in Chemacs

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,1 +1,4 @@
-(("default" . ((user-emacs-directory . "~/.emacs.default"))))
+(("default" . ((user-emacs-directory . "~/.emacs.default")))
+
+("spacemacs" . ((user-emacs-directory . "~/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d"))))))
