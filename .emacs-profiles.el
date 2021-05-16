b62d9bbf84e9c305da40c87a4e9925b589ac9fa2
Andreas Jonsson <ajdev8@gmail.com>
2021-05-16 21:32:25 +0200

    Fix spacemacs not initializing

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.emacs.default")))
 
-("spacemacs" . ((user-emacs-directory . "~/spacemacs")
+("spacemacs" . ((user-emacs-directory . "~/spacemacs/.emacs.d")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d"))))))
