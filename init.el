8c211ace59c7ab3f467b9c6c2dece629362e8de2
Andreas Jonsson <ajdev8@gmail.com>
2021-05-27 15:59:57 +0200

    Add TODO for my/kill-buffer-other-window

--- init.el
+++ init.el
@@ -599,6 +599,7 @@
     (switch-to-buffer nil)))   ; restore the original window in this part of the window
 
 ;; Kill buffer in other window
+;;   TODO: return focus to original window
 (defun my/kill-buffer-other-window ()
   "If there are multiple windows, then kill the buffer in the next window."
   (interactive)
