10d3eaa0354e319def81477ca119182dae9d25fc
Andreas Jonsson <ajdev8@gmail.com>
2021-06-16 15:07:52 +0200

    Show refined diffs for current hunk in magit

--- init.el
+++ init.el
@@ -450,6 +450,9 @@
 (use-package magit
   ;; Disable hl-line-mode
   :hook (magit-mode . (lambda () (setq-local global-hl-line-mode nil)))
+  :custom
+  ;; Show refined diffs for current hunk
+  (magit-diff-refine-hunk t)
   :bind (("C-x g" . magit-status)
          ;; ("C-x M-g" . magit-dispatch)   ; default binding
 	 ("C-c g" . magit-file-dispatch)))
