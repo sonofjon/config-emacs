7d69ae56432904e2b9ecfe03c16b1205aba9965e
Andreas Jonsson <ajdev8@gmail.com>
2021-06-30 15:41:03 +0200

    Add functions outline-show-more and outline-hide-more

--- init.el
+++ init.el
@@ -844,6 +844,12 @@
 
 ;;;; Outline
 
+(let ((map outline-minor-mode-map)) 
+  (define-key map (kbd "C-c <left>") 'outline-hide-more)
+  (define-key map (kbd "C-c <right>") 'outline-show-more)
+  (define-key map (kbd "C-c <up>") 'outline-previous-visible-heading)
+  (define-key map (kbd "C-c <down>") 'outline-next-visible-heading))
+
 ;;;; Navigation
 
 (windmove-default-keybindings 'ctrl)
@@ -1046,6 +1052,59 @@ Emacs session."
     (error "No recently-killed files to reopen")))
 
 ;;;; Outline
+
+(defun outline-body-p ()
+  (save-excursion
+    (outline-back-to-heading)
+    (outline-end-of-heading)
+    (and (not (eobp))
+         (progn (forward-char 1)
+                (not (outline-on-heading-p))))))
+
+(defun outline-body-visible-p ()
+  (save-excursion
+    (outline-back-to-heading)
+    (outline-end-of-heading)
+    (not (outline-invisible-p))))
+
+(defun outline-subheadings-p ()
+  (save-excursion
+    (outline-back-to-heading)
+    (let ((level (funcall outline-level)))
+      (outline-next-heading)
+      (and (not (eobp))
+           (< level (funcall outline-level))))))
+
+(defun outline-subheadings-visible-p ()
+  (interactive)
+  (save-excursion
+    (outline-next-heading)
+    (not (outline-invisible-p))))
+
+(defun outline-hide-more ()
+  (interactive)
+  (when (outline-on-heading-p)
+    (cond ((and (outline-body-p)
+                (outline-body-visible-p))
+           (hide-entry)
+           (hide-leaves))
+          (t
+           (hide-subtree)))))
+
+(defun outline-show-more ()
+  (interactive)
+  (when (outline-on-heading-p)
+    (cond ((and (outline-subheadings-p)
+                (not (outline-subheadings-visible-p)))
+           (show-children))
+          ((and (not (outline-subheadings-p))
+                (not (outline-body-visible-p)))
+           (show-subtree))
+          ((and (outline-body-p)
+                (not (outline-body-visible-p)))
+           (show-entry))
+          (t
+           (show-subtree)))))
 
 ;;;; Navigation
 
