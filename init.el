ab18a3fd2458a754a7bab66bac047b0eea38475b
Andreas Jonsson <ajdev8@gmail.com>
2021-06-30 13:20:23 +0200

    Add hydra for outline-minor-mode

--- init.el
+++ init.el
@@ -922,6 +922,7 @@
 
 ;;;; Hydras
 
+;;;;; Scrolling
 (defhydra hydra-scroll ()
   "Scrolling functions"
   ("<up>" scroll-down-line)
@@ -930,6 +931,7 @@
   ("<right>" scroll-up-paragraph))
 (global-set-key (kbd "C-c s") #'hydra-scroll/body)
 
+;;;;; Line navigation
 (defhydra hydra-next-line ()
   "Move to the next line or comment"
   ("<up>" my/previous-line)
@@ -937,6 +939,42 @@
   ("<left>" my/previous-comment)
   ("<right>" my/next-comment))
 (global-set-key (kbd "C-c n") #'hydra-next-line/body)
+
+;;;;; Outline 
+(defhydra hydra-outline (:color pink :hint nil)
+  "
+^Hide^             ^Show^           ^Move
+^^^^^^------------------------------------------------------
+_q_: sublevels     _a_: all         _u_: up
+_t_: body          _e_: entry       _n_: next visible
+_o_: other         _i_: children    _p_: previous visible
+_c_: entry         _k_: branches    _f_: forward same level
+_l_: leaves        _s_: subtree     _b_: backward same level
+_d_: subtree
+
+"
+  ;; Hide
+  ("q" hide-sublevels)    ; Hide everything but the top-level headings
+  ("t" hide-body)         ; Hide everything but headings (all body lines)
+  ("o" hide-other)        ; Hide other branches
+  ("c" hide-entry)        ; Hide this entry's body
+  ("l" hide-leaves)       ; Hide body lines in this entry and sub-entries
+  ("d" hide-subtree)      ; Hide everything in this entry and sub-entries
+  ;; Show
+  ("a" show-all)          ; Show (expand) everything
+  ("e" show-entry)        ; Show this heading's body
+  ("i" show-children)     ; Show this heading's immediate child sub-headings
+  ("k" show-branches)     ; Show all sub-headings under this heading
+  ("s" show-subtree)      ; Show (expand) everything in this heading & below
+  ;; Move
+  ("u" outline-up-heading)                ; Up
+  ("n" outline-next-visible-heading)      ; Next
+  ("p" outline-previous-visible-heading)  ; Previous
+  ("f" outline-forward-same-level)        ; Forward - same level
+  ("b" outline-backward-same-level)       ; Backward - same level
+  ("z" nil "leave"))
+
+(global-set-key (kbd "C-c #") 'hydra-outline/body)
 
 
 ;;;
