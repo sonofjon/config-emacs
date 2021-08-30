d9f112116217eaaefb4cc40c8c504cec8f0ac961
Andreas Jonsson <ajdev8@gmail.com>
2021-08-30 17:58:25 +0200

    Remove keybinding for consult-keep-lines

--- init.el
+++ init.el
@@ -482,7 +482,6 @@
          ("M-s l" . consult-line)
          ("M-s L" . consult-line-multi)
          ("M-s m" . consult-multi-occur)
-         ;; ("M-s k" . consult-keep-lines)
          ;; ("M-s u" . consult-focus-lines)
          ;; Isearch integration
          ("M-s e" . consult-isearch)
