5ecf498740d311b731ac1e6fa9b03c3348515848
Andreas Jonsson <ajdev8@gmail.com>
2022-07-07 03:28:13 +0200

    Configure Treemacs
    * Remove unnecessary variables
    * Remove progn statement
    * Don't check for git and python availability
    * Add descriptions for function calls in config section
    * Use treemacs-project-follow-mode, treemacs-indent-guide-mode and
      treemacs-git-commit-diff-mode

--- init.el
+++ init.el
@@ -851,74 +851,35 @@
   (which-key-add-key-based-replacements "C-c C-p" "treemacs")
   (which-key-add-key-based-replacements "C-c C-w" "treemacs")
   :config
-  (progn
-    (setq
-     ;; treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-     ;; treemacs-deferred-git-apply-delay        0.5
-     ;; treemacs-directory-name-transformer      #'identity
-     ;; treemacs-display-in-side-window          t
-     ;; treemacs-eldoc-display                   'simple
-     ;; treemacs-file-event-delay                5000
-     ;; treemacs-file-extension-regex            treemacs-last-period-regex-value
-     ;; treemacs-file-follow-delay               0.2
-     ;; treemacs-file-name-transformer           #'identity
-     ;; treemacs-follow-after-init               t
-     ;; treemacs-expand-after-init               t
-     ;; treemacs-find-workspace-method           'find-for-file-or-pick-first
-     ;; treemacs-git-command-pipe                ""
-     ;; treemacs-goto-tag-strategy               'refetch-index
-     ;; treemacs-indentation                     2
-     ;; treemacs-indentation-string              " "
-     ;; treemacs-is-never-other-window           nil
-     ;; treemacs-max-git-entries                 5000
-     ;; treemacs-missing-project-action          'ask
-     ;; treemacs-move-forward-on-expand          nil
-     ;; treemacs-no-png-images                   nil
-     ;; treemacs-no-delete-other-windows         t
-     ;; treemacs-project-follow-cleanup          nil
-     ;; treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-     ;; treemacs-position                        'left
-     ;; treemacs-read-string-input               'from-child-frame
-     ;; treemacs-recenter-distance               0.1
-     ;; treemacs-recenter-after-file-follow      nil
-     ;; treemacs-recenter-after-tag-follow       nil
-     ;; treemacs-recenter-after-project-jump     'always
-     ;; treemacs-recenter-after-project-expand   'on-distance
-     ;; treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-     ;; treemacs-show-cursor                     nil
-     ;; treemacs-show-hidden-files               t
-     ;; treemacs-silent-filewatch                nil
-     ;; treemacs-silent-refresh                  nil
-     ;; treemacs-sorting                         'alphabetic-asc
-     ;; treemacs-select-when-already-in-treemacs 'move-back
-     ;; treemacs-space-between-root-nodes        t
-     ;; treemacs-tag-follow-cleanup              t
-     ;; treemacs-tag-follow-delay                1.5
-     ;; treemacs-text-scale                      nil
-     ;; treemacs-user-mode-line-format           nil
-     ;; treemacs-user-header-line-format         nil
-     ;; treemacs-wide-toggle-width               70
-     treemacs-width                           30
-     ;; treemacs-width-increment                 1
-     ;; treemacs-width-is-initially-locked       t
-     ;; treemacs-workspace-switch-cleanup        nil)
-     )
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    ;; (treemacs-follow-mode t)
-    ;; (treemacs-filewatch-mode t)
-    ;; (treemacs-fringe-indicator-mode 'always)
-    ;; (treemacs-hide-gitignored-files-mode nil)
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-
+  (setq
+   treemacs-find-workspace-method           'find-for-file-or-manually-select
+   ;; treemacs-indentation                     2
+   ;; treemacs-indentation-string              " "
+   ;; treemacs-is-never-other-window           nil
+   ;; treemacs-no-delete-other-windows         t
+   ;; treemacs-project-follow-cleanup          nil
+   ;; treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+   ;; treemacs-position                        'left
+   ;; treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+   ;; treemacs-show-hidden-files               t
+   ;; treemacs-sorting                         'alphabetic-asc
+   treemacs-width                           30)
+  ;; Follow current buffer file in Treemacs window (on by default)
+  ;; (treemacs-follow-mode 1)
+  ;; Only display current project in Treemacs window
+  (treemacs-project-follow-mode 1)
+  ;; Show visual indicator in the fringe for highlighted file
+  (treemacs-fringe-indicator-mode 'always)
+  ;; Detect file system changes (on by default)
+  ;; (treemacs-filewatch-mode 1)
+  ;; Hide files ignored by Git
+  ;; (treemacs-hide-gitignored-files-mode 1)
+  ;; Display indentation guides
+  (treemacs-indent-guide-mode 1)
+  ;; Display git project annotations
+  (treemacs-git-commit-diff-mode 1)
+  ;; Highlight files using git status
+  (treemacs-git-mode 'deferred)
   :bind (("M-0" . treemacs-select-window)   ; TODO: this conflicts with C-u 0
          ("C-c t 1" . treemacs-delete-other-windows)
          ("C-c t t" . treemacs)
