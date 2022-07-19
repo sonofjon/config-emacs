c809d151078de408177e50937ee0c19e8ce5b965
Andreas Jonsson <ajdev8@gmail.com>
2022-07-19 16:08:29 +0200

    Add custom flyspell-correct-wrapper function

--- init.el
+++ init.el
@@ -827,8 +827,8 @@
   :after flyspell
   :bind (:map flyspell-mode-map
               ;; ("C-;" . flyspell-auto-correct-word)
-              ;; ("C-;" . (lambda () (interactive) (flyspell-correct-move (point) nil t)))
-              ("C-;" . (lambda () (interactive) (aj8/call-interactively-wih-prefix-toggle 'flyspell-correct-wrapper)))
+              ("C-;" . flyspell-correct-wrapper)
+              ([remap flyspell-correct-wrapper] . aj8/flyspell-correct-wrapper)
               ("C-c ," . my/flyspell-goto-previous-error)
               ("C-," . flyspell-correct-previous)
               ("C-c ." . flyspell-goto-next-error)
@@ -2599,6 +2599,19 @@ versa."
           (progn
             (message "No more miss-spelled word!")
             (setq arg 0))))))
+
+;; Custom arguments for flyspell-correct-wrapper
+(defun aj8/flyspell-correct-wrapper ()
+  "Correct the previous spelling error.  With a prefix argument correct
+the next error instead.  In either case rapid mode is enabled."
+  (interactive)
+  (cond
+   ((equal current-prefix-arg nil) ; no C-u
+    (flyspell-correct-move (point) nil t))
+   ((equal current-prefix-arg '(4)) ; C-u
+    (flyspell-correct-move (point) t t))
+   (t
+    (error "Unexpected input arguments"))))
 
 ;;;; Files
 
