2e1216a7f03ff8c88392bc993295e956a4a72296
Andreas Jonsson <ajdev8@gmail.com>
2022-08-29 16:36:47 +0200

    Use a combination of cape capf:s for completion

--- init.el
+++ init.el
@@ -464,15 +464,19 @@
          ("C-c u s" . cape-symbol))          ; complete symbols everywhere
   :init
   ;; Add `completion-at-point-functions', used by `completion-at-point'
+  ;; (add-to-list 'completion-at-point-functions #'cape-file)
+  ;; ;; (add-to-list 'completion-at-point-functions #'cape-history)
+  ;; (add-to-list 'completion-at-point-functions #'cape-line)
+  ;; (add-to-list 'completion-at-point-functions #'cape-ispell)
+  ;; ;; (add-to-list 'completion-at-point-functions #'cape-dict)
+  ;; (add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;; (add-to-list 'completion-at-point-functions #'cape-symbol)
+  ;; (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  ;; ;; (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;; Use a combination of capf:s instead
   (add-to-list 'completion-at-point-functions #'cape-file)
-  ;; (add-to-list 'completion-at-point-functions #'cape-history)
   (add-to-list 'completion-at-point-functions #'cape-line)
-  (add-to-list 'completion-at-point-functions #'cape-ispell)
-  ;; (add-to-list 'completion-at-point-functions #'cape-dict)
-  (add-to-list 'completion-at-point-functions #'cape-keyword)
-  (add-to-list 'completion-at-point-functions #'cape-symbol)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  ;; (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev+symbol+keyword+ispell)
   (which-key-add-key-based-replacements "C-c u" "corfu/cape"))   ; add label for prefix key
 
 ;; embark (conveniently act on minibuffer completions)
