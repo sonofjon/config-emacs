ef178a17cfb0034dfbeeec9891de59e83c28c4a3
Andreas Jonsson <ajdev8@gmail.com>
2022-12-03 18:13:57 +0100

    Remove keybinding for deprecated function consult-apropos

--- init.el
+++ init.el
@@ -454,7 +454,6 @@
          ("C-M-#" . consult-register)
          ;; Other custom bindings
          ;; ("M-y" . consult-yank-pop)           ; orig. yank-pop
-         ("<help> a" . consult-apropos)          ; orig. apropos-command
          ;; M-g bindings (goto-map)
          ("M-g g" . consult-goto-line)           ; orig. goto-line
          ("M-g M-g" . consult-goto-line)         ; orig. goto-line
