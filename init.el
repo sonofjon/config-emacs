5ec75bed5fce4df1d53f34acaf43d1e11b3c4f15
Andreas Jonsson <ajdev8@gmail.com>
2022-12-03 18:11:46 +0100

    Use manual preview for consult--source-file-register

--- init.el
+++ init.el
@@ -520,7 +520,8 @@
    :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep consult-bookmark
    consult-recent-file consult-xref consult--source-bookmark
-   consult--source-recent-file consult--source-project-recent-file
+   consult--source-file-register consult--source-recent-file
+   consult--source-project-recent-file
    :preview-key (kbd "M-`"))
 
   ;; Completion
