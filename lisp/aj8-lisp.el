eece2780b89b0e2c533b7084658932793715a9d3
Andreas Jonsson <ajdev8@gmail.com>
2024-03-18 20:07:06 +0100

    Simplify aj8/buffer-skip-regexp
    Simply skip all buffers starting with a "*".

--- lisp/aj8-lisp.el
+++ lisp/aj8-lisp.el
@@ -59,14 +59,9 @@ any window."
       (switch-to-buffer next-buffer))))
 
 (defcustom aj8/buffer-skip-regexp
-  (rx bos (or (or "*scratch*" "*Async-native-compile-log*" "*Dired log*")
-              (seq "*gptel-curl*" (zero-or-more anything))
-              (seq "magit-process" (zero-or-more anything))
-              (seq "*EGLOT" (zero-or-more anything) "events*"))
-      eos)
+    (rx bos "*")
   "Regular expression matching buffers ignored by `next-buffer'
-and `previous-buffer'. Note that there is no need to specify
-buffers that are displayed in side-windows."
+and `previous-buffer'."
   :type 'regexp
   :group 'aj8-lisp)
 
