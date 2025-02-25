4b7a278902a72a42e995dedf3dbac8991abf9df7
Andreas Jonsson <ajdev8@gmail.com>
2025-02-25 22:47:32 +0100

    init.el: hydra-smerge is not needed
    Use 'e' for Ediff instead.

--- init.el
+++ init.el
@@ -1679,11 +1679,6 @@ Elisp code explicitly in arbitrary buffers.")
   (setf (nth 4 magit-log-margin) 15)
   ;; Use abbreviated ages in log margin
   (setf (nth 1 magit-log-margin) 'age-abbreviated)
-  ;; TODO: Activate Smerge hydra on file visit in smerge-mode
-  ;;   Note that repeat-mode has already been added to smerge-basic-map in aj8-lisp.el
-  ;; (add-hook 'magit-diff-visit-file-hook (lambda ()
-  ;;                                         (when smerge-mode
-  ;;                                           (hydra-smerge/body))))
   ;; Clean up process buffers automatically
   (setq aj8/magit-cleanup-buffers t)
   ;; Hook for buffer cleanup
