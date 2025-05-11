725b28bb016a51fe80a37c53b4bee78e990b7e4b
Andreas Jonsson <ajdev8@gmail.com>
2025-05-11 17:10:13 +0200

    init.el: Configure magit-gptcommit

--- init.el
+++ init.el
@@ -1729,13 +1729,22 @@ Elisp code explicitly in arbitrary buffers.")
   (magit-delta-default-light-theme "OneHalfLight"))
 
 ;; magit-gptcommit (Git commit with help of GPT)
-;;   TODO: does not work
 (use-package magit-gptcommit
-  :disabled
   :demand t
   :after (gptel magit)
   :bind (:map git-commit-mode-map
-              ("C-c C-g" . magit-gptcommit-generate))
+              ("C-c C-g" . magit-gptcommit-generate)
+              ;; ("C-c C-g" . magit-gptcommit-commit-accept)
+              )
+  :init
+  (require 'llm-gemini)
+  :custom
+  (magit-gptcommit-llm-provider
+   (make-llm-gemini :key (auth-info-password
+                          (car (auth-source-search
+                                :host "generativelanguage.googleapis.com"
+                                :user "apikey")))
+                    :chat-model "gemini-2.5-flash-preview-04-17"))
   :config
   ;; Add gptcommit transient commands to `magit-commit'
   ;;   Eval (transient-remove-suffix 'magit-commit '(1 -1)) to remove gptcommit transient commands
