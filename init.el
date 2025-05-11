29971e636f23d4f31d1145fe1aa96fda564620d1
Andreas Jonsson <ajdev8@gmail.com>
2025-05-11 20:04:41 +0200

    init.el: Disable system-packages

--- init.el
+++ init.el
@@ -147,7 +147,7 @@
                           standard-themes
                           string-inflection
                           syntax-subword
-                          system-packages
+                          ;; system-packages   ; for use-package-ensure-system-package
                           transpose-frame
                           treesit-auto
                           undo-fu
