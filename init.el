888e5b87ef12e8f03a4915085718d9deddaf32ef
Andreas Jonsson <ajdev8@gmail.com>
2025-08-11 10:08:58 +0200

    init.el, aj8-hydra.el: Move hydra definitions to a separate file

--- init.el
+++ init.el
@@ -1910,7 +1910,14 @@ Elisp code explicitly in arbitrary buffers.")
 
 ;; hydra (make bindings that stick around)
 (use-package hydra
-  :defer)
+  :bind (("C-c y m" . hydra-markdown/body)
+         ("C-c y w" . hydra-window/body)
+         ("C-c y s" . hydra-scroll/body)
+         ("C-c y n" . hydra-navigation/body)
+         ("C-c y o" . hydra-outline/body)
+         ("C-c y e" . hydra-smerge/body))
+  :config
+  (require 'aj8-hydra))
 
 ;; keyfreq (track command frequencies)
 (use-package keyfreq
@@ -3020,7 +3027,6 @@ Elisp code explicitly in arbitrary buffers.")
     (add-to-list 'xref-prompt-for-identifier 'xref-find-references t)))
 
 
-
 ;;;;; CUSTOMIZATION
 
 ;;;; Variables
@@ -3089,239 +3095,6 @@ Elisp code explicitly in arbitrary buffers.")
 
 ;;; Editing
 
-;; Markdown
-(defhydra hydra-markdown (:hint nil :foreign-keys run)
-  "
-Format:         _B_: bold           _I_: italic
-                _Q_: quote          _C_: code           _P_: pre-formatted
-Headings:       _H_: automatic
-                _1_: h1       _2_: h2       _3_: h3       _4_: h4
-Lists:          _m_: new item
-Outline:        _k_: move up        _j_: move down
-                _h_: promote        _l_: demote
-Tables:         _a_: align          _s_: sort           _t_: transpose
-  Navigation    _n_: next row       _p_: previous row
-                _f_: forward cell   _b_: backward cell
-  Rows          ___: insert         _-_: delete
-  Columns       _|_: insert         _\\_: delete
-                _<_: move left      _>_: move right
-"
-  ("B" markdown-insert-bold)
-  ("I" markdown-insert-italic)
-  ("Q" markdown-insert-blockquote :color gray)
-  ("C" markdown-insert-code :color gray)
-  ("P" markdown-insert-pre :color gray)
-
-  ("H" markdown-insert-header-dwim)
-  ("1" markdown-insert-header-atx-1)
-  ("2" markdown-insert-header-atx-2)
-  ("3" markdown-insert-header-atx-3)
-  ("4" markdown-insert-header-atx-4)
-
-  ("m" markdown-insert-list-item)
-
-  ("h" markdown-promote)
-  ("l" markdown-demote)
-  ("k" markdown-move-up)
-  ("j" markdown-move-down)
-
-  ("a" markdown-table-align)
-  ("s" markdown-table-sort-lines)
-  ("t" markdown-table-transpose)
-  ("n" markdown-table-next-row)
-  ("p" aj8/markdown-table-prev-row)
-  ("f" markdown-table-forward-cell)
-  ("b" markdown-table-backward-cell)
-  ("_" markdown-table-insert-row)
-  ("-" markdown-table-delete-row)
-  ("|" markdown-table-insert-column)
-  ("\\" markdown-table-delete-column)
-  ("<" markdown-table-move-column-left)
-  (">" markdown-table-move-column-right)
-
-  ("q" nil "quit"))
-
-(keymap-global-set "C-c y m" #'hydra-markdown/body)
-
-;;; Windows
-(defhydra hydra-window (:hint nil)
-  "
-                                                                   ╭─────────┐
-  Select   Move   Resize    Split             Do                   │ Windows │
-╭──────────────────────────────────────────────────────────────────┴─────────┴─┐
-  ^ ^ _↑_ ^ ^   ^ ^ _↑_ ^ ^   ^ ^ _↑_ ^ ^   ╭─┬─┐ ^ ^           ╭─┬─┐ ^ ^
-  _←_ ^ ^ _→_   _←_ ^C^ _→_   _←_ ^M^ _→_   │ │ │[_h_]orizontal ├─┼─┤[_b_]alance  [_t_]ranspose
-  ^ ^ _↓_ ^ ^   ^ ^ _↓_ ^ ^   ^ ^ _↓_ ^ ^   ╰─┴─╯ ^ ^           ╰─┴─╯ ^ ^         fl[_i_]p (vertical)
-  ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^   ╭───┐ ^ ^           ╭───┐ ^ ^         fl[_o_]p (horizontal)
-  ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^   ├───┤[_v_]ertical   │   │[_z_]oom     [_r_]otate
-  ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^   ╰───╯ ^ ^           ╰───╯ ^ ^
-╰──────────────────────────────────────────────────────────────────────────────╯
-↺ [_u_]ndo layout | ↻ re[_s_]set layout
-⇋ [_n_]ext window | ✗ [_c_]lose window
-"
-  ;; ("<tab>" hydra-master/body "back")
-  ("q" nil "quit")
-  ("<left>" windmove-left)
-  ("<right>" windmove-right)
-  ("<up>" windmove-up)
-  ("<down>" windmove-down)
-  ("C-<left>" windmove-swap-states-left)
-  ("C-<right>" windmove-swap-states-right)
-  ("C-<up>" windmove-swap-states-up)
-  ("C-<down>" windmove-swap-states-down)
-  ;; ("S-<left>" shrink-window-horizontally)
-  ;; ("S-<right>" enlarge-window-horizontally)
-  ;; ("S-<up>" shrink-window)
-  ;; ("S-<down>" enlarge-window)
-  ("M-<left>" (lambda ()
-                (interactive)
-                (my/move-splitter-left 1)
-                (message "Width: %s Height: %s"
-                         (window-body-width) (window-body-height))))
-  ("M-<right>" (lambda ()
-                (interactive)
-                (my/move-splitter-right 1)
-                (message "Width: %s Height: %s"
-                         (window-body-width) (window-body-height))))
-  ;; ("M-<up>" my/move-splitter-up)
-  ("M-p" (lambda ()
-                (interactive)
-                (my/move-splitter-up 1)
-                (message "Width: %s Height: %s"
-                         (window-body-width) (window-body-height))))
-  ;; ("M-<down>" my/move-splitter-down)
-  ("M-n" (lambda ()
-                (interactive)
-                (my/move-splitter-down 1)
-                (message "Width: %s Height: %s"
-                         (window-body-width) (window-body-height))))
-  ("v" split-window-vertically)
-  ("h" split-window-horizontally)
-  ("b" balance-windows)
-  ("z" delete-other-windows)
-  ("u" winner-undo)
-  ("s" winner-redo)
-  ("c" delete-window)
-  ("n" other-window)
-  ("t" transpose-frame)
-  ("i" flip-frame)
-  ("o" flop-frame)
-  ("r" rotate-frame-clockwise))
-
-(keymap-global-set "C-c y w" #'hydra-window/body)
-
-;;; Navigation
-
-;; Scrolling
-(defhydra hydra-scroll (:hint nil)
-  "
-^Line^            ^Paragraph^
-^^^^---------------------------
-_↑_: previous     _←_: previous
-_↓_: next         _→_: next
-
-Scroll by line or paragraph.
-"
-  ("q" nil "quit")
-  ("<up>" scroll-down-line)
-  ("<down>" scroll-up-line)
-  ;; ("<left>" aj8/scroll-down-paragraph)
-  ("<left>" (lambda ()
-              (interactive)
-              (aj8/scroll-down-paragraph 1)))
-  ;; ("<right>" aj8/scroll-up-paragraph))
-  ("<right>" (lambda ()
-               (interactive)
-               (aj8/scroll-up-paragraph 1))))
-
-(keymap-global-set "C-c y s" #'hydra-scroll/body)
-
-;; Line navigation
-(defhydra hydra-navigation (:hint nil)
-  "
-^Line^            ^Comment^
-^^^^---------------------------
-_↑_: previous     _←_: previous
-_↓_: next         _→_: next
-
-Move to the next line or comment.
-"
-  ("q" nil "quit")
-  ("<up>" aj8/previous-line)
-  ("<down>" aj8/next-line)
-  ("<left>" aj8/previous-comment)
-  ("<right>" aj8/next-comment))
-
-(keymap-global-set "C-c y n" #'hydra-navigation/body)
-
-;;; Outline
-(defhydra hydra-outline (:color pink :hint nil)
-  "
-Hide & Show         ^^Move
-------------------------------------------------------
-  _←_ hide               _↑_ previous visible
-  _→_ show               _↓_ next visible
-C-_←_ hide (global)    C-_↑_ backward same level
-C-_→_ show (global)    C-_↓_ forward same level
-  ^ ^                  M-_↑_ up
-
-Hide, show and navigate outlines.
-"
-  ;; Hide and show
-  ("<left>" my/outline-hide-more)
-  ("<right>" my/outline-show-more)
-  ("C-<left>" outline-hide-sublevels)
-  ("C-<right>" outline-show-all)
-  ;; Move
-  ("<up>" outline-previous-visible-heading)
-  ("<down>" outline-next-visible-heading)
-  ("C-<up>" outline-backward-same-level)
-  ("C-<down>" outline-forward-same-level)
-  ("M-p" outline-up-heading)
-  ;; Quit
-  ("q" nil "quit"))
-
-(keymap-global-set "C-c y o" #'hydra-outline/body)
-
-;;; Version control
-
-;; Smerge
-;;   Reference: https://github.com/alphapapa/unpackaged.el?tab=readme-ov-file#smerge-mode
-(defhydra hydra-smerge
-  (:color pink :hint nil :post (smerge-auto-leave))
-  "
-^Move^       ^Keep^               ^Diff^                 ^Other^
-^^-----------^^-------------------^^---------------------^^-------
-_n_ext       _b_ase               _<_: upper/base        _C_ombine
-_p_rev       _u_pper              _=_: upper/lower       _r_esolve
-^^           _l_ower              _>_: base/lower        _k_ill current
-^^           _a_ll                _R_efine
-^^           _RET_: current       _E_diff
-"
-  ("n" smerge-next)
-  ("p" smerge-prev)
-  ("b" smerge-keep-base)
-  ("u" smerge-keep-upper)
-  ("l" smerge-keep-lower)
-  ("a" smerge-keep-all)
-  ("RET" smerge-keep-current)
-  ("\C-m" smerge-keep-current)
-  ("<" smerge-diff-base-upper)
-  ("=" smerge-diff-upper-lower)
-  (">" smerge-diff-base-lower)
-  ("R" smerge-refine)
-  ("E" smerge-ediff)
-  ("C" smerge-combine-with-next)
-  ("r" smerge-resolve)
-  ("k" smerge-kill-current)
-  ("ZZ" (lambda ()
-          (interactive)
-          (save-buffer)
-          (bury-buffer))
-   "Save and bury buffer" :color blue)
-  ("q" nil "cancel" :color blue))
-
-(keymap-global-set "C-c y e" #'hydra-smerge/body)
 
 ;;;;; LATE SETTINGS
 
