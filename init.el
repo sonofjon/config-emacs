e9d900cfd7f51723bc01579ebe342ef60fa8ea15
Andreas Jonsson <ajdev8@gmail.com>
2025-10-26 19:40:33 +0100

    init.el: Remove obsolete TODO about TRAMP bash history

--- init.el
+++ init.el
@@ -1057,9 +1057,6 @@
   ;; (add-to-list 'auto-mode-alist '("\\.tex\\'" . latex-ts-mode)))
 
 ;; tramp (Transparent Remote (file) Access, Multiple Protocol)
-;; TODO: Prevent TRAMP commands from appearing in bash history
-;;       (e.g. "exec env TERM dumb INSIDE_EMACS ENV HISTFILE ~/.tramp_history bash history")
-;;       https://mail.gnu.org/archive/html/tramp-devel/2024-11/msg00002.html
 ;; TODO: Disable tramp debug buffer
 (use-package tramp
   :ensure nil   ; don't install built-in packages
