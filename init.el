3f07dbd0a7fc85153a896eecf706e475063017f5
Andreas Jonsson <ajdev8@gmail.com>
2025-11-02 21:26:03 +0100

    init.el: Use Ediff with Monet

--- init.el
+++ init.el
@@ -1351,6 +1351,9 @@
   ;; Disable prefix key
   (monet-prefix-key nil))
   ;; (monet-prefix-key "C-c a C-m"))
+  ;; Use Ediff
+  ;; (monet-diff-tool #'monet-ediff-tool)   ; nil for disable
+  ;; (monet-diff-cleanup-tool #'monet-ediff-cleanup-tool))
 
 ;; claude-code (Claude Code Emacs integration)
 (use-package claude-code
