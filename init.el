25576ed5d5142d2fde0a0f255b16f9eaef7f0779
Andreas Jonsson <ajdev8@gmail.com>
2026-01-13 14:23:04 +0100

    init.el: Replace obsolete consult-source--* symbols

--- init.el
+++ init.el
@@ -2096,9 +2096,9 @@ the window so that the streaming position appears near the bottom."
    consult-theme
    :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep consult-bookmark
-   consult-recent-file consult-xref consult--source-bookmark
-   consult--source-file-register consult--source-recent-file
-   consult--source-project-recent-file
+   consult-recent-file consult-xref consult-source-bookmark
+   consult-source-file-register consult-source-recent-file
+   consult-source-project-recent-file
    :preview-key "C-c M-p")
 
   ;; Completion
