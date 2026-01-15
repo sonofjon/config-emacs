48fd107e6a6d42c45d8da329beeedb2797a55944
Andreas Jonsson <ajdev8@gmail.com>
2026-01-16 00:59:45 +0100

    init.el: Update minibuffer side-window mode package name

--- init.el
+++ init.el
@@ -178,7 +178,7 @@
         (hideshow-cycle :url "https://github.com/sonofjon/hideshow-cycle.el" :rev :newest)
         (markdown-links :url "https://github.com/sonofjon/markdown-links.el.git" :rev :newest)
         (mcp-server :url "https://github.com/rhblind/emacs-mcp-server.git" :rev :newest)
-        (minibuffer-side-window :url "https://github.com/sonofjon/minibuffer-side-window.el.git" :rev :newest)
+        (minibuffer-side-window-mode :url "https://github.com/sonofjon/minibuffer-side-window-mode.el.git" :rev :newest)
         (monet :url "https://github.com/stevemolitor/monet.git" :rev :newest)
         (obsidian-yaml-tools :url "https://github.com/sonofjon/obsidian-yaml-tools.el.git")
         (reflow :url "https://github.com/sonofjon/reflow.el" :rev :newest)
@@ -2432,8 +2432,8 @@ Elisp code explicitly in arbitrary buffers.")
   ;; Enable M-x minibuffer-hide-completions (make function interactive)
   (put 'minibuffer-hide-completions 'interactive-form '(interactive)))
 
-;; minibuffer-side-window (side-window display for minibuffer packages)
-(use-package minibuffer-side-window
+;; minibuffer-side-window-mode (side-window display for minibuffer packages)
+(use-package minibuffer-side-window-mode
   :after (vertico embark which-key)
   :config
   (minibuffer-side-window-mode 1))
